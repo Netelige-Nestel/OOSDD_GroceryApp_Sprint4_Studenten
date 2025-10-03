2dd17d066b77067a25d8ac764d0eb0e01f52f430
Fedde Talma <ftalma@protonmail.com>
2025-10-03 13:53:11 +0200

    Best verkochte producten overzicht

--- Grocery.Core/Services/GroceryListItemsService.cs
+++ Grocery.Core/Services/GroceryListItemsService.cs
@@ -51,9 +51,35 @@ namespace Grocery.Core.Services
 
         public List<BestSellingProducts> GetBestSellingProducts(int topX = 5)
         {
-            throw new NotImplementedException();
-        }
+            var groceryItems = _groceriesRepository.GetAll();
 
+            var bestSellingProducts = groceryItems
+                .GroupBy(item => item.ProductId)
+                .Select(group =>
+                {
+                    var product = _productRepository.Get(group.Key);
+                    return new BestSellingProducts(
+                        group.Key,
+                        product?.Name ?? "Unknown",
+                        product?.Stock ?? 0,
+                        group.Sum(g => g.Amount),
+                        0
+                    );
+                })
+                .OrderByDescending(p => p.NrOfSells)
+                .Take(topX)
+                .ToList();
+
+            List<BestSellingProducts> rankedProducts = bestSellingProducts
+                .Select((p, index) =>
+                {
+                    p.Ranking = index + 1;
+                    return p;
+                })
+                .ToList();
+
+            return rankedProducts;
+        }
         private void FillService(List<GroceryListItem> groceryListItems)
         {
             foreach (GroceryListItem g in groceryListItems)
